93e24e71f1870827fe1d3a77ef5a8ecee159dffe
patrickbjohnson <pbj@progresslabs.co>
2019-05-08 17:56:56 -0400

    remove query calls

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,13 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
-import Hero from '../components/hero'
 import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    // const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    // const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+    // const [author] = get(this, 'props.data.allContentfulPerson.edges')
 
     return (
       <Layout location={this.props.location} >
