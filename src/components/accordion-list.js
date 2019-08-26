7c034642de41b3772ae426673441b04f4cd5d837
patrickbjohnson <pbj@progresslabs.co>
2019-08-26 14:57:32 -0400

    wip

---
+++ src/components/accordion-list.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Accordion from './accordion'
+import SectionHeader from './section-header'
+
+const AccordionList = (props) => {
+  const { 
+    sectionTitle, 
+    activeJobs 
+  } = props
+  
+  return (
+    <div>
+      {sectionTitle &&
+        <SectionHeader text={sectionTitle} />
+      }
+      <Accordion set={activeJobs}/>
+    </div>
+  )
+}
+
+export default AccordionList
--- src/templates/page.js
+++ src/templates/page.js
@@ -7,11 +7,12 @@ import Footer from '../components/site-footer'
 import ContentBlockGrid from '../components/ContentGrid'
 import ContentHero from '../components/content-hero'
 import TextBlockGrid from '../components/text-block-grid'
-import JobList from '../components/job-list'
+import AccordionList from '../components/accordion-list'
 import FeaturedPosts from '../components/featured-posts'
 import HomeHero from '../components/home-hero'
 import HeroSlider from '../components/multi-slide-hero'
 import MobileContentGrid from '../components/MobileContentGrid'
+import GoodPeople from '../components/GoodPeople'
 import MediaQuery from 'react-responsive'
 
 import base from '../components/base.css'
@@ -36,7 +37,12 @@ const Page = (props) => {
             <div style={{'position': 'relative', 'zIndex': 2}}>
               {components && components.map(component => {
                   const type = cleanComponentName( component.__typename );
+                  console.log(type, component)
                   switch ( type ) {
+                    case 'GoodPeople' :
+                      return <GoodPeople
+                        key={component.id}
+                        {...component} />
                     case 'FeaturedPosts':
                       return <FeaturedPosts
                         key={component.id}
@@ -65,7 +71,11 @@ const Page = (props) => {
                             key={component.id}
                             {...component} />
                     case 'JobList':
-                        return <JobList
+                        return <AccordionList
+                            key={component.id}
+                            {...component} />
+                    case 'AccordionList':
+                        return <AccordionList
                             key={component.id}
                             {...component} />
                     case 'HeroSlider':
@@ -197,6 +207,18 @@ export const pageQuery = graphql`
               }
             }
           }
+          
+          ... on ContentfulAccordionList {
+            id
+            activeJobs {
+              id
+              title
+              description {
+                id
+                description
+              }
+            }
+          }
           ... on ContentfulHeroSlider {
             id
             heroSlides {
@@ -207,7 +229,21 @@ export const pageQuery = graphql`
               }
               slideImage {
                 fluid {
-                    ...GatsbyContentfulFluid
+                  ...GatsbyContentfulFluid
+                }
+              }
+            }
+          }
+          ... on ContentfulGoodPeople {
+            title
+            id
+            blocks {
+              personBio {
+                personBio
+              }
+              personImage {
+                fluid {
+                  ...GatsbyContentfulFluid
                 }
               }
             }
