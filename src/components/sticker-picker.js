1dbc2de6d069cbf9cf201148aabb29c9e4a9706c
Ryan Dunnewold <rmdunnewold@gmail.com>
2019-10-11 11:58:54 -0500

    Fix document issue

--- src/components/sticker-picker.js
+++ src/components/sticker-picker.js
@@ -124,6 +124,7 @@ const StickerPicker = () => {
     })
 
     const showButton = useMemo(() => {
+      if(!document) return false
       return y < .25 * document.body.scrollHeight || y === 0
     }, [y])
 
