70d21c9263a8cae56ca4d16b5d8b893e40268d29
Patrick Johnson <pbj@progresslabs.co>
2019-12-07 12:49:00 -0500

    update section title parallax

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Parallax } from 'react-scroll-parallax'
 import MediaQuery from 'react-responsive'
 
 import styles from './section-header.module.css'
 
 class SectionHeader extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        const { 
-            text, 
-            classes,
-            uniqueID
-        } = this.props
+  render() {
+    const { text, classes, uniqueID } = this.props
 
-        return (
-            <div>
-                <MediaQuery minWidth={768}>
-                    <Parallax className={classes} y={['100px', '-20px']}>
-                        <div className={styles.block}>
-                            <h2 className={styles.text} data-id={uniqueID}>{text}</h2>
-                        </div>
-                    </Parallax>
-                </MediaQuery>
-                <MediaQuery maxWidth={767}>
-                  <div className={classes}>
-                      <div className={styles.block}>
-                          <h2 className={styles.text} data-id={uniqueID}>{text}</h2>
-                      </div>
-                  </div>
-                </MediaQuery>
+    return (
+      <div>
+        <MediaQuery minWidth={768}>
+          <Parallax className={classes} y={['80px', '-20px']}>
+            <div className={styles.block}>
+              <h2 className={styles.text} data-id={uniqueID}>
+                {text}
+              </h2>
             </div>
-        )
-    }
+          </Parallax>
+        </MediaQuery>
+        <MediaQuery maxWidth={767}>
+          <div className={classes}>
+            <div className={styles.block}>
+              <h2 className={styles.text} data-id={uniqueID}>
+                {text}
+              </h2>
+            </div>
+          </div>
+        </MediaQuery>
+      </div>
+    )
+  }
 }
 
 export default SectionHeader
