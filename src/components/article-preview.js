f52a04ba7e3ac62aa53c00b4dc4fc7f519484cb7
Patrick Johnson <pbj@progresslabs.co>
2020-03-29 16:48:56 -0400

    update styles

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -6,41 +6,43 @@ import VideoPlayer from './video-player'
 
 import styles from './article-preview.module.css'
 
+const formatDate = date => {
+  const d = new Date(date)
+  const rawDay = d.getDate()
+  const rawMonth = d.getMonth()
+  const rawYear = d.getFullYear()
 
-const formatDate = (date) => {
-  const d = new Date(date);
-  const rawDay = d.getDate();
-  const rawMonth = d.getMonth();
-  const rawYear = d.getFullYear();
-
-  const day = (rawDay > 10) ? rawDay : "0" + rawDay;
-  const month = (rawMonth > 10) ? rawMonth : "0" + rawMonth;
+  const day = rawDay >= 10 ? rawDay : '0' + rawDay
+  const month = rawMonth >= 10 ? rawMonth : '0' + rawMonth
   const year = rawYear.toString().substr(-2)
-  return `${day}.${month}.${year}`;
+  return `${month}.${day}.${year}`
 }
 
 export default ({ article }) => {
   const { node } = article
 
   return (
-  <article className={styles.article} id={node.slug}>
-    <div className={styles.meta}>
-      <h2 className={styles.title}>{node.title}</h2>
-      <small>{formatDate(node.publishDate)}</small>
-    </div>
-    {node.video && 
-      <VideoPlayer videoUrl={node.video.videoUrl}/>
-    }
-    {(!node.video && node.image) &&
-      <Img className={styles.image} alt={node.title} fluid={node.image.fluid} />
-    }
-    
-    <div className={styles.content}>
-    <Markdown 
-      className={styles.body} 
-      source={node.body.body} 
-      escapeHtml={false}
-    />
-    </div>
-  </article>
-)}
+    <article className={styles.article} id={node.slug}>
+      <div className={styles.meta}>
+        <h2 className={styles.title}>{node.title}</h2>
+        <small>{formatDate(node.publishDate)}</small>
+      </div>
+      {node.video && <VideoPlayer videoUrl={node.video.videoUrl} />}
+      {!node.video && node.image && (
+        <Img
+          className={styles.image}
+          alt={node.title}
+          fluid={node.image.fluid}
+        />
+      )}
+
+      <div className={styles.content}>
+        <Markdown
+          className={styles.body}
+          source={node.body.body}
+          escapeHtml={false}
+        />
+      </div>
+    </article>
+  )
+}
