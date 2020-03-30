d21ad85b8c376a397f69c87782ba5f7f597927d3
Patrick Johnson <pbj@progresslabs.co>
2020-03-30 18:36:11 -0400

    enable html entities

--- src/components/content-hero.js
+++ src/components/content-hero.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from 'react'
 import Markdown from 'react-markdown'
 import styles from './content-hero.module.css'
 
 import Transition from './transition'
 
 function ContentHero(props) {
-    const {
-        heroTitle,
-        heroContent,
-        backgroundColor,
-    } = props
+  const { heroTitle, heroContent, backgroundColor } = props
 
-    return (
-        <div className={styles.block} style={{
-            backgroundColor: backgroundColor ? backgroundColor : '#fff'
-        }}>
-            <div className={styles.inner}>
-                <Transition className={styles.hero}>
-                  <Markdown source={heroTitle.heroTitle} />
-                </Transition>
-                <Transition delay={250} className={styles.body}>
-                  <Markdown source={heroContent.heroContent} />
-                </Transition>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className={styles.block}
+      style={{
+        backgroundColor: backgroundColor ? backgroundColor : '#fff',
+      }}
+    >
+      <div className={styles.inner}>
+        <Transition className={styles.hero}>
+          <Markdown source={heroTitle.heroTitle} escapeHtml={false} />
+        </Transition>
+        <Transition delay={250} className={styles.body}>
+          <Markdown source={heroContent.heroContent} escapeHtml={false} />
+        </Transition>
+      </div>
+    </div>
+  )
 }
 
 export default ContentHero
